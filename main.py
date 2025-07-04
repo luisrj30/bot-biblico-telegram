4c465d577fefee5a9b27508be9788fad13ed8448
luisrj30 <50125328+luisrj30@users.noreply.github.com>
2025-07-03 22:49:40 -0300

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from telegram import Update
 from telegram.ext import Application, CommandHandler, MessageHandler, ContextTypes, filters
 
 load_dotenv()  # Carrega o conteúdo do arquivo .env
-BOT_TOKEN = os.getenv(7855633786:AAHH0nTE2Rk4RJEuXf0i7LM7YO9q9V3KZ4o)  # Token agora vem da variável de ambiente
+BOT_TOKEN = os.getenv("7855633786:AAHH0nTE2Rk4RJEuXf0i7LM7YO9q9V3KZ4o")  # Token agora vem da variável de ambiente
 
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
